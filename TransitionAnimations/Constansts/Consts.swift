96227a68ebac6f80a0208a3b06714d7b27209a66
VitaliZahar <vitalizahar@gmail.com>
2018-12-02 15:15:19 -0800

    Setup project structure

---
+++ TransitionAnimations/Constansts/Consts.swift
@@ -0,0 +1,13 @@
+//
+//  Consts.swift
+//  TransitionAnimations
+//
+//  Created by vitali on 12/2/18.
+//  Copyright © 2018 vitcopr. All rights reserved.
+//
+
+import Foundation
+
+struct Consts {
+    
+}
