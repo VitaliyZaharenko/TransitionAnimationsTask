4125acff963a39ab9b14d3fa73b68cfba966eb5f
VitaliZahar <vitalizahar@gmail.com>
2018-12-13 10:32:58 -0800

    Start Implementing transition list

---
+++ TransitionAnimations/Controllers/AnimationListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TransitionAnimations
+//
+//  Created by vitali on 12/2/18.
+//  Copyright © 2018 vitcopr. All rights reserved.
+//
+
+import UIKit
+
+fileprivate struct Const {
+    
+    struct FadeInFadeOutItem {
+        static let index = 0
+    }
+    
+    
+}
+
+class AnimationListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch indexPath.row {
+        case Const.FadeInFadeOutItem.index:
+            showFadeInFadeOut()
+        default:
+            return
+        }
+    }
+}
+
+
+//MARK: - Private Helper Methods
+
+extension AnimationListViewController {
+    
+    
+    func showFadeInFadeOut(){
+        print("Show fadeinout")
+    }
+    
+}
+
